be3bad74c74958bd873a40eaffbcc87da77db6cc
neetance <ankitbasu14@gmail.com>
2025-02-01 23:39:57 +0530

    Method to register for whitelist

--- app/src/launchToken.ts
+++ app/src/launchToken.ts
@@ -1,114 +1,113 @@
-import Web3, { Transaction } from "web3";
-import dotenv from "dotenv";
-import { max } from "simple-statistics";
+// import Web3, { Transaction } from "web3";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-interface IParams {
-  name: string;
-  symbol: string;
-  tokenAddr: string;
-  totalSupply: number;
-  supportPeriod: number;
-  minSupportContrib: number;
-  maxSupportContrib: number;
-  minContrib: number;
-  maxContrib: number;
-  salePeriod: number;
-  maxSupporters: number;
-}
+// interface IParams {
+//   name: string;
+//   symbol: string;
+//   tokenAddr: string;
+//   totalSupply: number;
+//   supportPeriod: number;
+//   minSupportContrib: number;
+//   maxSupportContrib: number;
+//   minContrib: number;
+//   maxContrib: number;
+//   salePeriod: number;
+//   maxSupporters: number;
+// }
 
-export const launchNewToken = async (params: IParams) => {
-  try {
-    const {
-      name,
-      symbol,
-      tokenAddr,
-      totalSupply,
-      supportPeriod,
-      minSupportContrib,
-      maxSupportContrib,
-      minContrib,
-      maxContrib,
-      salePeriod,
-      maxSupporters,
-    } = params;
-    if (!window.ethereum) alert("Please install metamask");
+// export const launchNewToken = async (params: IParams) => {
+//   try {
+//     const {
+//       name,
+//       symbol,
+//       tokenAddr,
+//       totalSupply,
+//       supportPeriod,
+//       minSupportContrib,
+//       maxSupportContrib,
+//       minContrib,
+//       maxContrib,
+//       salePeriod,
+//       maxSupporters,
+//     } = params;
+//     if (!window.ethereum) alert("Please install metamask");
 
-    const web3 = new Web3(window.ethereum);
-    const accounts = await web3.eth.getAccounts();
-    const userAddress = accounts[0];
+//     const web3 = new Web3(window.ethereum);
+//     const accounts = await web3.eth.getAccounts();
+//     const userAddress = accounts[0];
 
-    const factoryAddr = process.env.FACTORY_ADDRESS;
-    const factoryAbi = [
-      {
-        inputs: [
-          { internalType: "string", name: "name", type: "string" },
-          { internalType: "string", name: "symbol", type: "string" },
-          { internalType: "address", name: "tokenAddr", type: "address" },
-          { internalType: "uint256", name: "totalSupply", type: "uint256" },
-          { internalType: "uint256", name: "supportPeriod", type: "uint256" },
-          {
-            internalType: "uint256",
-            name: "minSupportContrib",
-            type: "uint256",
-          },
-          {
-            internalType: "uint256",
-            name: "maxSupportContrib",
-            type: "uint256",
-          },
-          { internalType: "uint256", name: "minContrib", type: "uint256" },
-          { internalType: "uint256", name: "maxContrib", type: "uint256" },
-          { internalType: "uint256", name: "salePeriod", type: "uint256" },
-          { internalType: "uint256", name: "maxSupporters", type: "uint256" },
-        ],
-        name: "newInstance",
-        outputs: [
-          { internalType: "address", name: "", type: "address" },
-          { internalType: "address", name: "", type: "address" },
-        ],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-    ];
+//     const factoryAddr = process.env.FACTORY_ADDRESS;
+//     const factoryAbi = [
+//       {
+//         inputs: [
+//           { internalType: "string", name: "name", type: "string" },
+//           { internalType: "string", name: "symbol", type: "string" },
+//           { internalType: "address", name: "tokenAddr", type: "address" },
+//           { internalType: "uint256", name: "totalSupply", type: "uint256" },
+//           { internalType: "uint256", name: "supportPeriod", type: "uint256" },
+//           {
+//             internalType: "uint256",
+//             name: "minSupportContrib",
+//             type: "uint256",
+//           },
+//           {
+//             internalType: "uint256",
+//             name: "maxSupportContrib",
+//             type: "uint256",
+//           },
+//           { internalType: "uint256", name: "minContrib", type: "uint256" },
+//           { internalType: "uint256", name: "maxContrib", type: "uint256" },
+//           { internalType: "uint256", name: "salePeriod", type: "uint256" },
+//           { internalType: "uint256", name: "maxSupporters", type: "uint256" },
+//         ],
+//         name: "newInstance",
+//         outputs: [
+//           { internalType: "address", name: "", type: "address" },
+//           { internalType: "address", name: "", type: "address" },
+//         ],
+//         stateMutability: "nonpayable",
+//         type: "function",
+//       },
+//     ];
 
-    const contract = new web3.eth.Contract(factoryAbi, factoryAddr);
-    const tx = contract.methods.newInstance(
-      name,
-      symbol,
-      tokenAddr,
-      totalSupply,
-      supportPeriod,
-      minSupportContrib,
-      maxSupportContrib,
-      minContrib,
-      maxContrib,
-      salePeriod,
-      maxSupporters
-    );
-    const txData = tx.encodeABI();
-    const txGas = await tx.estimateGas({ from: userAddress });
+//     const contract = new web3.eth.Contract(factoryAbi, factoryAddr);
+//     const tx = contract.methods.newInstance(
+//       name,
+//       symbol,
+//       tokenAddr,
+//       totalSupply,
+//       supportPeriod,
+//       minSupportContrib,
+//       maxSupportContrib,
+//       minContrib,
+//       maxContrib,
+//       salePeriod,
+//       maxSupporters
+//     );
+//     const txData = tx.encodeABI();
+//     const txGas = await tx.estimateGas({ from: userAddress });
 
-    const transaction = {
-      from: userAddress,
-      to: factoryAddr,
-      data: txData,
-      gas: txGas,
-    };
+//     const transaction = {
+//       from: userAddress,
+//       to: factoryAddr,
+//       data: txData,
+//       gas: txGas,
+//     };
 
-    web3.eth
-      .sendTransaction(transaction)
-      .on("transactionHash", (hash: string) => {
-        console.log("Transaction sent. Hash:", hash);
-      })
-      .on("confirmation", (receipt: any) => {
-        console.log("Transaction confirmed. Receipt:", receipt);
-      })
-      .on("error", (error: Error) => {
-        console.error("Transaction failed:", error);
-      });
-  } catch (err) {
-    console.log(err);
-  }
-};
+//     web3.eth
+//       .sendTransaction(transaction)
+//       .on("transactionHash", (hash: string) => {
+//         console.log("Transaction sent. Hash:", hash);
+//       })
+//       .on("confirmation", (receipt: any) => {
+//         console.log("Transaction confirmed. Receipt:", receipt);
+//       })
+//       .on("error", (error: Error) => {
+//         console.error("Transaction failed:", error);
+//       });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
---
+++ app/src/whitelistRegister.ts
@@ -0,0 +1,65 @@
+import Web3 from "web3";
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
+
+const register = async (userAddress: string, symbol: string) => {
+  try {
+    const whitelistAddr = process.env.WHITELIST_ADDRESS;
+    const whitelistAbi = [
+      {
+        inputs: [
+          { internalType: "bytes32", name: "_commitment", type: "bytes32" },
+        ],
+        name: "register",
+        outputs: [{ internalType: "bool", name: "", type: "bool" }],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+    ];
+
+    const provider = new Web3.providers.HttpProvider(process.env.RPC_URL!);
+    const web3 = new Web3(provider);
+    const whitelist = new web3.eth.Contract(whitelistAbi, whitelistAddr);
+
+    const secret = process.env.SECRET!;
+    const commitment = web3.utils.soliditySha3(secret, userAddress);
+
+    const tx = whitelist.methods.register(commitment);
+    const txData = tx.encodeABI();
+    const txGas = await tx.estimateGas({ from: userAddress });
+
+    const senderAddress = process.env.SENDER_ADDRESS!;
+    const senderPrivateKey = process.env.SENDER_PRIVATE_KEY!;
+
+    const transaction = {
+      from: senderAddress,
+      to: whitelistAddr,
+      data: txData,
+      gas: txGas,
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(
+      transaction,
+      senderPrivateKey
+    );
+    const receipt = await web3.eth.sendSignedTransaction(
+      signedTx.rawTransaction!
+    );
+
+    console.log("Transaction receipt: ", receipt);
+    if (receipt.events) {
+      const event = receipt.events.Registered;
+      if (event) {
+        console.log("User registered:", event.returnValues);
+        const apiUrl = "";
+        await axios.post(apiUrl); // post method to add the user commitment to the merkle tree in the database
+      }
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+export default register;
