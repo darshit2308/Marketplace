42aee03a91cd3d1b131f9bf162a0b35e8a52fb06
neetance <ankitbasu14@gmail.com>
2025-02-05 23:15:26 +0530

    Added the zkverify contract

---
+++ contracts/src/verifyZKProof.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract VerifyZKProof {
+    bytes32 public constant PROVING_SYSTEM_ID =
+        keccak256(abi.encodePacked("groth16"));
+
+    address public immutable zkvContract;
+    bytes32 public immutable vkHash;
+
+    event SuccessfulProofSubmission(address indexed from);
+
+    constructor(address _zkvContract, bytes32 _vkHash) {
+        zkvContract = _zkvContract;
+        vkHash = _vkHash;
+    }
+
+    function verifyZKProof(
+        uint256 attestationId,
+        bytes32[] calldata merklePath,
+        uint256 leafCount,
+        uint256 index,
+        address inputAddress
+    ) external {
+        require(
+            _verifyProofHasBeenPostedToZkv(
+                attestationId,
+                inputAddress,
+                merklePath,
+                leafCount,
+                index
+            )
+        );
+        emit SuccessfulProofSubmission(inputAddress);
+    }
+
+    function _verifyProofHasBeenPostedToZkv(
+        uint256 attestationId,
+        address inputAddress,
+        bytes32[] calldata merklePath,
+        uint256 leafCount,
+        uint256 index
+    ) internal view returns (bool) {
+        bytes memory encodedInput = abi.encodePacked(
+            _changeEndianess(uint256(uint160(inputAddress)))
+        );
+        bytes32 leaf = keccak256(
+            abi.encodePacked(PROVING_SYSTEM_ID, vkHash, keccak256(encodedInput))
+        );
+
+        (bool callSuccessful, bytes memory validProof) = zkvContract.staticcall(
+            abi.encodeWithSignature(
+                "verifyProofAttestation(uint256,bytes32,bytes32[],uint256,uint256)",
+                attestationId,
+                leaf,
+                merklePath,
+                leafCount,
+                index
+            )
+        );
+
+        require(callSuccessful);
+
+        return abi.decode(validProof, (bool));
+    }
+
+    function _changeEndianess(uint256 input) internal pure returns (uint256 v) {
+        v = input;
+        // swap bytes
+        v =
+            ((v &
+                0xFF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00) >>
+                8) |
+            ((v &
+                0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF) <<
+                8);
+        // swap 2-byte long pairs
+        v =
+            ((v &
+                0xFFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000) >>
+                16) |
+            ((v &
+                0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF) <<
+                16);
+        // swap 4-byte long pairs
+        v =
+            ((v &
+                0xFFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000) >>
+                32) |
+            ((v &
+                0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF) <<
+                32);
+        // swap 8-byte long pairs
+        v =
+            ((v &
+                0xFFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF0000000000000000) >>
+                64) |
+            ((v &
+                0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF) <<
+                64);
+        // swap 16-byte long pairs
+        v = (v >> 128) | (v << 128);
+    }
+}
