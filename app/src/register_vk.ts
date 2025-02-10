f3c21fdec09d5862045f9b0363bdc1c27c0d2ed2
neetance <ankitbasu14@gmail.com>
2025-02-11 02:06:41 +0530

    Generated vk hashes

--- app/src/register_vk.ts
+++ app/src/register_vk.ts
@@ -1,36 +1,52 @@
 import { readFileSync } from "fs";
-import { zkVerifySession } from "zkverifyjs";
+import { zkVerifySession, Library, CurveType } from "zkverifyjs";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const run = async () => {
-  const vkWhitelist = JSON.parse(
-    readFileSync("../../zkp/setup/whitelist_verification_key.json", "utf-8")
-  );
-  const vkContribution = JSON.parse(
-    readFileSync("../../zkp/setup/contribution_verification_key.json", "utf-8")
-  );
+  try {
+    console.log("reading files...");
+    const vkWhitelist = JSON.parse(
+      readFileSync(
+        "../zkp/circuits/setup/whitelist_verification_key.json",
+        "utf-8"
+      )
+    );
+    const vkContribution = JSON.parse(
+      readFileSync(
+        "../zkp/circuits/setup/contribution_verification_key.json",
+        "utf-8"
+      )
+    );
 
-  const session = await zkVerifySession
-    .start()
-    .Custom(process.env.ZKV_RPC_URL)
-    .withAccount(process.env.ZKV_SEED_PHRASE!);
-  const { transactionResult: whitelistTransactionResult } = await session
-    .registerVerificationKey()
-    .groth16()
-    .execute(vkWhitelist);
-  const { statementHash: whitelistVKHash } = await whitelistTransactionResult;
+    console.log(vkWhitelist);
 
-  const { transactionResult: contributionTransactionResult } = await session
-    .registerVerificationKey()
-    .groth16()
-    .execute(vkContribution);
-  const { statementHash: contributionVKHash } =
-    await contributionTransactionResult;
+    console.log("starting zkv session...");
+    const session = await zkVerifySession
+      .start()
+      .Custom(process.env.ZKV_RPC_URL)
+      .withAccount(process.env.ZKV_SEED_PHRASE!);
 
-  console.log("Whitelist verification key hash:", whitelistVKHash);
-  console.log("Contribution verification key hash:", contributionVKHash);
+    console.log("session started, sending transactions");
+    const { transactionResult: whitelistTransactionResult } = await session
+      .registerVerificationKey()
+      .groth16(Library.snarkjs, CurveType.bn128)
+      .execute(vkWhitelist);
+    const { statementHash: whitelistVKHash } = await whitelistTransactionResult;
+
+    const { transactionResult: contributionTransactionResult } = await session
+      .registerVerificationKey()
+      .groth16(Library.snarkjs, CurveType.bn128)
+      .execute(vkContribution);
+    const { statementHash: contributionVKHash } =
+      await contributionTransactionResult;
+
+    console.log("Whitelist verification key hash:", whitelistVKHash);
+    console.log("Contribution verification key hash:", contributionVKHash);
+  } catch (err) {
+    console.log("error:", err);
+  }
 };
 
 run()
