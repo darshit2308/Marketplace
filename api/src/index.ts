5a51e296382c75594d94781e99a4e69d0d8c2bba
neetance <ankitbasu14@gmail.com>
2025-02-15 23:58:44 +0530

    minor changes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,4 @@
-/* Installed dependecies:
-    npm i express
-    npm i nodemon
-    npm install -g typescript
-    npm install --save-dev typescript
-    tsc --init  -> Generated tsconfig.json file
-    npm install --save-dev @types/express  -> Installs the remaining depednicies for typescript on express
-    npm install mongoose dotenv -> For mongodb
-    touch .env -> Created .env file
-    npm install --save-dev ts-node  -> Very Very important , to run npm start
-*/
-
-import * as bodyParser from "body-parser"; //  imports all exports from the body-parser module and assigns them to the bodyParser object.
+import * as bodyParser from "body-parser";
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -20,13 +8,11 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(bodyParser.json()); // Adds the bodyParser middleware
-const port: number = 8000;
+app.use(bodyParser.json());
+const port = process.env.PORT || 8000;
 app.use(cors());
 
-// MongoDB connection
-const MONGODB_URI = process.env.MONGODB_URI!; /* process.env is a global object 
-containing all environment variables */
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 mongoose
   .connect(MONGODB_URI)
@@ -37,16 +23,11 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
-// Root route
 app.use("/api", router);
 app.get("/", (req, res) => {
-  // Defins the server
   res.send("Hello from the Server");
 });
 
-// Add a commitment to the leaves array
-// Note: If we remove bodyParser , the req.body becomes undefined .
-// Starts the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running`);
 });
